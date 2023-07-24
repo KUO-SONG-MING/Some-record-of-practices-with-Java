982569e73b32eb107bee3ce44497a232f9aecbe7
KUO-SONG-MING <70509190+KUO-SONG-MING@users.noreply.github.com>
2023-07-24 17:00:25 +0800

    Product functionality-Set the return time zone and time format."

--- web-demo/src/main/java/com/martin/webdemo/rowmapper/ProductRowMapper.java
+++ web-demo/src/main/java/com/martin/webdemo/rowmapper/ProductRowMapper.java
@@ -18,8 +18,8 @@ public class ProductRowMapper implements RowMapper<Product> {
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
         product.setDescription(resultSet.getString("description"));
-        product.setCreatedDate(resultSet.getDate("created_date"));
-        product.setLastModifiedDate(resultSet.getDate("last_modified_date"));
+        product.setCreatedDate(resultSet.getTimestamp("created_date"));
+        product.setLastModifiedDate(resultSet.getTimestamp("last_modified_date"));
 
         return product;
     }
