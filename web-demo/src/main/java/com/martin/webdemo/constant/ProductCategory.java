18bf09f143e1e1613698ba471ff24e282ecf085b
KUO-SONG-MING <70509190+KUO-SONG-MING@users.noreply.github.com>
2023-07-24 16:45:56 +0800

    Product functionality-Refactor Category using ENUM type."

---
+++ web-demo/src/main/java/com/martin/webdemo/constant/ProductCategory.java
@@ -0,0 +1,7 @@
+package com.martin.webdemo.constant;
+
+public enum ProductCategory {
+    FOOD,
+    CAR,
+    E_BOOK
+}
--- web-demo/src/main/java/com/martin/webdemo/rowmapper/ProductRowMapper.java
+++ web-demo/src/main/java/com/martin/webdemo/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.martin.webdemo.rowmapper;
 
+import com.martin.webdemo.constant.ProductCategory;
 import com.martin.webdemo.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -12,7 +13,7 @@ public class ProductRowMapper implements RowMapper<Product> {
         Product product = new Product();
         product.setProductId(resultSet.getInt("product_id"));
         product.setProductName(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+        product.setCategory(ProductCategory.valueOf(resultSet.getString("category")));
         product.setImageUrl(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
